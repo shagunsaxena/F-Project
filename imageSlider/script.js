9da06bb1fd0585d7e624d3576ecf8b821bbb24b0
Shagun Saxena <shagunsaxena96@gmail.com>
2026-06-03 23:31:29 +0530

    commiting code for imageSlide with html, css, js, image

---
+++ imageSlider/script.js
@@ -0,0 +1,33 @@
+const sliderTabs = document.querySelectorAll(".slider-tab");
+const sliderIndicator = document.querySelector(".slider-indicator");
+
+const updateIndicator = (tab,index)=>{
+    sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
+    sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+};
+const swiper = new Swiper(".slider-container",{
+    effect: "fade",
+    speed: 1300,
+    // autoplay:{ delay: 4000}
+    on: {
+        slideChange: () => {
+            const currentTabIndex =[...sliderTabs].indexOf(
+                sliderTabs[swiper.activeIndex]);
+            updateIndicator(sliderTabs[swiper.activeIndex],currentTabIndex);
+            
+        }
+    }
+});
+ 
+ 
+sliderTabs.forEach((tab,index)=>{
+    tab.addEventListener("click",()=>{ 
+        swiper.slideTo(index);
+        updateIndicator(tab,index);
+    });
+});
+
+updateIndicator(sliderTabs[0],0);
+window.addEventListener("resize",()=>{
+    updateIndicator(sliderTabs[swiper.activeIndex],0);
+});
